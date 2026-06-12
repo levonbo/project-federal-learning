27b421e4653fc6dcaf55568fc897a3c03d3d573a
Levon Boos <levon.boos@outlook.de>
2026-06-12 19:38:45 +0200

    optuna for gpu

--- param_optimizer.py
+++ param_optimizer.py
@@ -8,20 +8,22 @@ import dataset
 import numpy as np
 from medmnist import Evaluator
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def objective(trial):
     optimizer_name = trial.suggest_categorical("Optimizer", ["Adam", "RMSprop", "SGD"])
     learning_rate = trial.suggest_float('lr', 1e-4, 1e-1, log=True)
     BATCH_SIZE = trial.suggest_categorical('Batch Size', [16, 32, 64, 128])
 
-    mnist_datasets = ["pathmnist", "chestmnist","dermamnist", "octmnist", "pneumoniamnist", "retinamnist", "breastmnist", "bloodmnist", "tissuemnist","organamnist", "organcmnist","organsmnist"]
+    mnist_datasets = ["retinamnist", "chestmnist","dermamnist", "octmnist", "pneumoniamnist", "pathmnist", "breastmnist", "bloodmnist", "tissuemnist","organamnist", "organcmnist","organsmnist"]
     dataset_scores = []
     for data_flag in mnist_datasets:
         _, task, _, _,_ = config.get_info(data_flag)
-        model = models.get_model(config.param.model_name, data_flag)
+        model = models.get_model(config.param.model_name, data_flag).to(device)
 
         optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=learning_rate)
 
-        train_loader, validation_loader, _ = dataset.get_loader(data_flag, config.param.model_name, BATCH_SIZE, config.param.download, config.param.size)    
+        train_loader, validation_loader, _ = dataset.get_loader(data_flag, config.param.model_name, BATCH_SIZE, config.param.download, config.param.size)
 
         if task == "multi-label, binary-class":
             criterion = nn.BCEWithLogitsLoss()
@@ -31,6 +33,7 @@ def objective(trial):
         for _ in range(2):
             model.train()
             for inputs, targets in train_loader:
+                inputs, targets = inputs.to(device), targets.to(device)
                 optimizer.zero_grad()
                 outputs = model(inputs)
 
@@ -44,24 +47,25 @@ def objective(trial):
                     outputs = outputs.softmax(dim=-1)
 
                 loss.backward()
-                optimizer.step() 
+                optimizer.step()
             model.eval()
             y_score = torch.tensor([])
             with torch.no_grad():
                 for inputs, targets in validation_loader:
+                    inputs = inputs.to(device)
                     outputs = model(inputs)
                     if task == 'multi-label, binary-class':
                         outputs = outputs.softmax(dim=-1)
                     else:
                         outputs = outputs.softmax(dim=-1)
 
-                    y_score = torch.cat((y_score, outputs), 0)
+                    y_score = torch.cat((y_score, outputs.cpu()), 0)
 
             evaluator = Evaluator(data_flag, "val")
             metrics = evaluator.evaluate(y_score)
             auc, acc = metrics
             score = 0.7 * auc + 0.3 * acc
-        dataset_scores.append(score)
+        dataset_scores.append(score) #type: ignore
     return np.mean(dataset_scores)
 
 study = optuna.create_study(direction='maximize')
